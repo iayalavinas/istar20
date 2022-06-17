8bca4a62404613305c495b42943b53757ef7cd84
iayalavinas <101105124+iayalavinas@users.noreply.github.com>
2022-06-17 11:31:09 +0200

    Add files via upload
    Main file that accepts two input arguments.

--- iStar20/src/uma/istar/Main.java
+++ iStar20/src/uma/istar/Main.java
@@ -4,12 +4,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		if(args.length==0) {
-			System.out.println("No input arguments");
+		if(args.length!=2) {
+			System.out.println("To call this program uses two arguments: piStar model in JSON and the name of the script");
 		}else{
 			IStarModel model=new IStarModel();
 			model.parseModel(args[0]);	
-			model.generateIntLinProg("prueba_1");
+			model.generateIntLinProg(args[1]);
 		}
 	}
 
